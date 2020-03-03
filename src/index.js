1eaeb4ac1fb02fa7b116d2940837a08241a94270
ushachev <vit.ush@mail.ru>
2020-03-03 17:40:21 +0300

    add interface option

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const program = new commander.Command();
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format');
 
 export default program;
