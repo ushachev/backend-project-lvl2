8e3b7ac26213a8e2eab5a9a0ccbc6620362032ff
ushachev <vit.ush@mail.ru>
2020-03-21 16:12:50 +0300

    fix tests duplication

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,49 +4,25 @@ import genDiff from '../src';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const getDiff = (extension, format = 'pretty') => {
+const getDiff = (extension, format) => {
   const pathToFile1 = getFixturePath(`before.${extension}`);
   const pathToFile2 = getFixturePath(`after.${extension}`);
 
   return genDiff(pathToFile1, pathToFile2, format);
 };
 
-let expectedPretty;
-let expectedPlain;
-let expectedJson;
+let expected;
 
-beforeAll(() => {
-  expectedPretty = fs.readFileSync(getFixturePath('pretty.txt'), 'utf8');
-  expectedPlain = fs.readFileSync(getFixturePath('plain.txt'), 'utf8');
-  expectedJson = fs.readFileSync(getFixturePath('output.json'), 'utf8');
-});
-
-describe('pretty genDiff', () => {
-  test.each([
-    ['json'],
-    ['yml'],
-    ['ini'],
-  ])('from %s', (extension) => {
-    expect(getDiff(extension)).toEqual(expectedPretty);
+describe.each([
+  ['pretty', 'pretty.txt'],
+  ['plain', 'plain.txt'],
+  ['json', 'output.json'],
+])('%s genDiff', (format, fixture) => {
+  beforeAll(() => {
+    expected = fs.readFileSync(getFixturePath(fixture), 'utf8');
   });
-});
 
-describe('plain genDiff', () => {
-  test.each([
-    ['json'],
-    ['yml'],
-    ['ini'],
-  ])('from %s', (extension) => {
-    expect(getDiff(extension, 'plain')).toEqual(expectedPlain);
-  });
-});
-
-describe('json genDiff', () => {
-  test.each([
-    ['json'],
-    ['yml'],
-    ['ini'],
-  ])('from %s', (extension) => {
-    expect(getDiff(extension, 'json')).toEqual(expectedJson);
+  test.each(['json', 'yml', 'ini'])('from %s', (extension) => {
+    expect(getDiff(extension, format)).toEqual(expected);
   });
 });
